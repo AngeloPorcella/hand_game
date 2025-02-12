586db397bee1b8a4685eb77d530e94383728499a
Angelo Porcella <angeloporcella1@gmail.com>
2025-02-12 08:42:21 -0700

    Added start screen to select difficulty

--- hand_game.py
+++ hand_game.py
@@ -11,7 +11,6 @@ mp_drawing = mp.solutions.drawing_utils
 
 shapes = []
 
-
 # Start Video Feed
 cap = cv2.VideoCapture(0)
 
@@ -43,10 +42,83 @@ def check_collision(hull, shape):
     return cv2.pointPolygonTest(hull, (shape[0], shape[1]), False) >= 0
 
 
-def run():
+def start_screen():
+    # Menu shapes
+    start_screen_shapes = []
+    left_width = int(frame_width * 0.1)
+    right_width = int(frame_width * 0.78)
+    height = int(frame_height * 0.5)
+    radius = int(frame_width / 30)
+    timer = 0
+    start_screen_shapes.append((left_width, height, radius, timer))
+    start_screen_shapes.append((right_width, height, radius, timer))
+    start_screen_shapes.append((int(frame_width * 0.45), int(frame_height * 0.75), radius, timer))
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb_frame)
+        # Store hand shape
+        hulls = []
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                # Get the landmark coordinates
+                points = np.array(
+                    [[int(point.x * frame.shape[1]), int(point.y * frame.shape[0])] for point in
+                     hand_landmarks.landmark])
+                # Draw the convex hull
+                hull = cv2.convexHull(points)
+                cv2.fillPoly(frame, [hull], color=(0, 0, 255))
+                hulls.append(hull)
+        left = start_screen_shapes[0]
+        right = start_screen_shapes[1]
+        close = start_screen_shapes[2]
+
+        for hull in hulls:
+            # Increment score if player hits circle
+            if check_collision(hull, left):
+                return 1
+            elif check_collision(hull, right):
+                return 2
+            elif check_collision(hull, close):
+                return 0
+        else:
+            # Draw the circles and make the titles
+            cv2.circle(frame, (left[0], left[1]), left[2], (0, 255, 0), -1)
+            cv2.putText(frame, f"Easy", (left[0] - int(0.5 * left[2]), left[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.circle(frame, (right[0], right[1]), right[2], (255, 0, 255), -1)
+            cv2.putText(frame, f"Hard", (right[0] - int(0.5 * right[2]), right[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.circle(frame, (close[0], close[1]), close[2], (0, 0, 255), -1)
+            cv2.putText(frame, f"Exit", (close[0] - int(0.5 * right[2]), close[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        # cv2.circle(frame, (left[0], left[1]), left[2], (255, 0, 255), -1)
+        # cv2.circle(frame, (right[0], right[1]), right[2], (255, 0, 255), -1)
+
+        cv2.imshow('Hand Detection', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return 0
+
+
+
+def run(difficulty):
     win_flag = False
     start_time = None
     score = 0
+    timer_diff = None
+    score_diff = None
+    if difficulty == 1:
+        timer_diff = 100
+        score_diff = 15
+    elif difficulty == 2:
+        timer_diff = 50
+        score_diff = 25
+    elif difficulty == 0:
+        return
+    if timer_diff is None:
+        return
     # Game loop
     while True:
         ret, frame = cap.read()
@@ -85,7 +157,7 @@ def run():
                 shapes.remove(shape)
                 # Add new shape in
                 shapes.append(shape_new)
-                if timer >= 75:
+                if timer >= timer_diff:
                     # If timer expires remove the shape without increasing score
                     shapes.remove(shape_new)
                     continue
@@ -101,7 +173,7 @@ def run():
         cv2.putText(frame, f"Score: {score}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
         # Win condition
-        if score >= 10:
+        if score >= score_diff:
             if not win_flag:
                 start_time = time.time()
             win_flag = True
@@ -121,4 +193,5 @@ def run():
     cv2.destroyAllWindows()
 
 if __name__=="__main__":
-    run()
+    difficulty = start_screen()
+    run(difficulty)
